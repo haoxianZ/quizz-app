9a6790ee14e90366de6193c617900acd2423707e
John Zhang <johnhaoxian@gmail.com>
2020-07-02 11:07:44 -0400

    basic setup

---
+++ index.js
@@ -0,0 +1,29 @@
+const STORE = [{
+    question: 'The tallest building in the world is located in which city?'
+    ,answer:'Dubai',
+    options: ['Dubai', 'A','B','c']
+},
+{
+    question: 'Which year was the original Toy Story film released in the US?',
+    answer:'1995',
+    options: ['1995', 'A','B','c']
+
+},
+{ question: 'Name the current UK Home Secretary.',
+    answer:'Priti Patel',
+    options: ['Priti Patel', 'A','B','c']
+},
+{question: 'In 2017 the Best Picture Oscar winner was erroneously announced as La La Land. But which film actually won the award?',
+answer:'Moonlight',
+options: ['Moonlight', 'A','B','c']
+},
+{question: 'Name the longest river in the UK.',
+answer:'River Severn',
+options: ['River Severn', 'A','B','c']
+},
+{question: 'What is the capital city of Ukraine?',
+answer: 'Kiev',
+options: ['Kiev', 'A','B','c']
+}
+];
+
